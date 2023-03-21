4fb4cb83446c32a4fb91a528c60e9bb24bf16e26
Geraldoviananeto <128382517+Geraldoviananeto@users.noreply.github.com>
2023-03-21 11:47:59 -0300

    Add files via upload

--- aula06.java
+++ aula06.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 
 public class aula06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("insira um numero x");
+		int nx = sc.nextInt();
+
+		System.out.println(" Insira um numero y ");
+		int ny = sc.nextInt();
+
+		if (nx > ny) {
+			System.out.println(nx + "É maior que" + ny);
+		} else {
+			if (ny > nx) {
+				System.out.println(ny + "É maior que " + nx);
+			} else {
+				System.out.println("São iguais ");
+			}
+
+		}
 
 	}
 
