8c7164f1d4d321899ae1c6384281062dd3541f0f
Geraldoviananeto <128382517+Geraldoviananeto@users.noreply.github.com>
2023-04-04 09:47:56 -0300

    Add files via upload

---
+++ projeto22.java
@@ -0,0 +1,28 @@
+package projeto02;
+
+import java.util.Scanner;
+
+public class projeto22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Informe um valor ");
+		int valor1 = sc.nextInt();
+		
+		if (valor1 > 100) {
+			if (valor1 <200) {
+				System.out.println("Está no intervalo ");
+				
+			} else {
+
+			}
+			
+		} else {
+
+		}
+		
+		
+	}
+
+}
